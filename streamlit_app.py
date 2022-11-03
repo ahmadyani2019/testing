3029c56f804aa0fdc533027150aa165128339519
ahmadyani2019 <90377183+ahmadyani2019@users.noreply.github.com>
2022-11-03 20:52:33 +0700

    updatenew

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,23 @@
-# Libraries to be used ------------------------------------------------------------
+# Requirements yang dibutuhkan ------------------------------------------------------------
 
 import streamlit as st
 import requests
 import json
 import os
+from pytube import YouTube
+import librosa
+import soundfile as sf
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+from pydub import AudioSegment
 
-# title and favicon ------------------------------------------------------------
+# Mengatur judul dan ikon pada situs Streamlit ------------------------------------------------------------
 
 st.set_page_config(
     page_title="Audio dan Video Transcription App", page_icon="📝", layout="wide"
 )
 
-# App layout width -------------------------------------------------
+# Mengatur layout pada situs Streamlit -------------------------------------------------
 
 
 def _max_width_():
@@ -30,12 +36,13 @@ def _max_width_():
 
 _max_width_()
 
-# multi navbar -------------------------------------------------
+# Mengatur menu pada situs Streamlit -------------------------------------------------
 
 
 def main():
     pages = {
-        "👾 Audio Transcriber": demo,
+        "🔊 Audio Transcriber": demo,
+        "📹 Video Transcription": vid,
     }
 
     if "page" not in st.session_state:
@@ -131,9 +138,86 @@ def demo():
         path_in = None
         st.stop()
 
-# Notes about the app -------------------------------------------------
+# Video Transcriber from Youtube -------------------------------------------------
 
 
+def vid():
+    c1, c2, c3 = st.columns([1, 4, 1])
+    with c2:
+
+        with st.form(key="my_form"):
+
+            link = st.text_input('YouTube URL')
+
+            st.info(
+                f"""
+                        👆 Insert YouTube URL.
+                        """
+            )
+
+            submit_button = st.form_submit_button(label="Transcribe")
+
+    if link is not None:
+        path_in = link.name
+        audio_path = 'C://'
+        try:
+            yt = YouTube(link)
+        except:
+            st.success('Connection Error!')
+        yt.streams.filter(file_extension='mp4')
+        stream = yt.streams.get_by_itag(139)
+        stream.download(audio_path, "output.mp4")
+        given_audio = AudioSegment.from_file('C://output.mp4', format="mp4")
+        given_audio.export("output.wav", format="wav")
+        sp, rate = sf.read("output.wav")
+        sp = librosa.resample(sp.T, rate, 16000)
+        sf.write("output.wav", sp.T, 16000, subtype='PCM_24')
+
+        tokenizer = Wav2Vec2Tokenizer.from_pretrained(
+            "indonesian-nlp/wav2vec2-large-xlsr-indonesian")
+        model = Wav2Vec2ForCTC.from_pretrained(
+            "indonesian-nlp/wav2vec2-large-xlsr-indonesian")
+
+        stream = librosa.stream(
+            "output.wav",
+            block_length=5,
+            frame_length=16000,
+            hop_length=16000
+        )
+        for speech in stream:
+            if len(speech.shape) > 1:
+                speech = speech[:, 0] + speech[:, 1]
+
+            input_values = tokenizer(speech, return_tensors="pt").input_values
+            logits = model(input_values).logits
+
+            predicted_ids = torch.argmax(logits, dim=-1)
+            transcription = tokenizer.decode(predicted_ids[0])
+            st.success(transcription)
+
+            c0, c1 = st.columns([2, 2])
+
+            with c0:
+                st.download_button(
+                    "Download the transcription",
+                    transcription,
+                    file_name=None,
+                    mime=None,
+                    key=None,
+                    help=None,
+                    on_click=None,
+                    args=None,
+                    kwargs=None,
+                )
+    else:
+        path_in = None
+        st.warning(
+            "🚨 Program Error"
+        )
+        st.stop()
+
+
+    # Catatan tambahan -------------------------------------------------
 with st.expander("ℹ️ - About this app", expanded=False):
 
     st.write(
